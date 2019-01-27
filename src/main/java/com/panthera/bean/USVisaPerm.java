a09897b1be4c6552158d9b2c0495562268eeee98
Mahesh <Patelmahesh1618@gmail.com>
2019-01-27 18:51:06 +0400

    ok

---
+++ src/main/java/com/panthera/bean/USVisaPerm.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.panthera.bean;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Mahesh
+ */
+public class USVisaPerm  implements Serializable{
+    
+}
